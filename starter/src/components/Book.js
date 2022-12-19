be4f00acda0019c6568d9c4f79d79f2804b60f97
Yingke Yu <yy2792@columbia.edu>
2022-12-19 14:08:50 -0500

    Update Book.js

--- starter/src/components/Book.js
+++ starter/src/components/Book.js
@@ -5,21 +5,23 @@ import IBook from "../common/Models";
 
 const Book = ({ book, onShelfUpdate }) => {
   return (
-    <div className="book">
-      <div className="book-top">
-        <div
-          className="book-cover"
-          style={{
-            width: 128,
-            height: 193,
-            backgroundImage: `url(${book.imageLinks})`,
-          }}
-        ></div>
-        <BookShelfChanger book={book} onShelfUpdate={onShelfUpdate} />
+    <li key={book.id}>
+      <div className="book">
+        <div className="book-top">
+          <div
+            className="book-cover"
+            style={{
+              width: 128,
+              height: 193,
+              backgroundImage: `url(${book?.imageLinks?.thumbnail})`,
+            }}
+          ></div>
+          <BookShelfChanger book={book} onShelfUpdate={onShelfUpdate} />
+        </div>
         <div className="book-title">{book?.title}</div>
         <div className="book-authors">{book?.authors?.join(", ")}</div>
       </div>
-    </div>
+    </li>
   );
 };
 
